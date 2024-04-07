0eca0dffc9a59c049474a739b518e182bd0666fa
Spongebob229 <yyytimon@gmail.com>
2024-04-07 22:20:25 +0500

    :)

---
+++ Jina/Jina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jina
+//
+//  Created by Schannikov Timothy on 07.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jina/Jina/JinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JinaApp.swift
+//  Jina
+//
+//  Created by Schannikov Timothy on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct JinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
