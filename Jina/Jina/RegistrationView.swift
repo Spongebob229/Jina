ec38218d54718a797e1346e15ceb21a2d100867d
Spongebob229 <yyytimon@gmail.com>
2024-04-09 22:32:29 +0500

    Fixed signup

--- Jina/Jina/RegistrationView.swift
+++ Jina/Jina/RegistrationView.swift
@@ -8,90 +8,95 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var name = ""
+    @State private var surname = ""
     @State private var email = ""
     @State private var password = ""
     @State var showingAlert = false
     @State var loginError: Error?
     
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("Create account")
-                    .padding(.bottom, 12)
-                    .font(.system(size: 30, weight: .semibold))
-                Spacer()
-            }
-            .padding(.top)
-            .padding(.bottom, 8)
-            
-            HStack {
-                Text("Name")
-                    .padding(.bottom, 12)
-                    .font(.system(size: 17))
-                Spacer()
-            }
-            InputField(text: $email, placeholder: "Your name")
-                .padding(.bottom, 16)
-            
-            HStack {
-                Text("Surname")
-                    .padding(.bottom, 12)
-                    .font(.system(size: 17))
-                Spacer()
-            }
-            InputField(text: $email, placeholder: "Your surname")
-                .padding(.bottom, 16)
-            
-            
-            HStack {
-                Text("Email")
-                    .padding(.bottom, 12)
-                    .font(.system(size: 17))
-                Spacer()
-            }
-            InputField(text: $email, placeholder: "Your email")
-                .padding(.bottom, 16)
-            
-            HStack {
-                Text("Password")
-                    .padding(.bottom, 12)
-                    .font(.system(size: 17))
-                Spacer()
-            }
-            
-            SecureInputField(text: $password)
-                .padding(.bottom, 24)
-            
-            HStack {
-                Text("Already have an account?")
-                Button {
-                    
-                } label: {
-                    Text("LOG IN")
-                        .font(.system(size: 17, weight: .bold))
-                        .foregroundStyle(Color.blue)
+        ScrollView {
+            VStack(spacing: 0) {
+                HStack {
+                    Text("Create account")
+                        .padding(.bottom, 12)
+                        .font(.system(size: 30, weight: .semibold))
+                    Spacer()
                 }
-                Spacer()
-            }
-            
-            Spacer()
-            
-            Spacer()
-            
-            MainButton(title: "Sign up") {
-                Task {
-                    do {
-                        try await AuthService.shared.signInUser(with: email, password: password)
-                        print("Logged in")
-                    } catch {
-                        loginError = error
-                        showingAlert.toggle()
+                .padding(.top)
+                .padding(.bottom, 8)
+                
+                HStack {
+                    Text("Name")
+                        .padding(.bottom, 12)
+                        .font(.system(size: 17))
+                    Spacer()
+                }
+                InputField(text: $name, placeholder: "Your name")
+                    .padding(.bottom, 16)
+                
+                HStack {
+                    Text("Surname")
+                        .padding(.bottom, 12)
+                        .font(.system(size: 17))
+                    Spacer()
+                }
+                InputField(text: $surname, placeholder: "Your surname")
+                    .padding(.bottom, 16)
+                
+                
+                HStack {
+                    Text("Email")
+                        .padding(.bottom, 12)
+                        .font(.system(size: 17))
+                    Spacer()
+                }
+                InputField(text: $email, placeholder: "Your email")
+                    .padding(.bottom, 16)
+                
+                HStack {
+                    Text("Password")
+                        .padding(.bottom, 12)
+                        .font(.system(size: 17))
+                    Spacer()
+                }
+                
+                SecureInputField(text: $password)
+                    .padding(.bottom, 24)
+                
+                HStack {
+                    Text("Already have an account?")
+                    Button {
+                        
+                    } label: {
+                        Text("LOG IN")
+                            .font(.system(size: 17, weight: .bold))
+                            .foregroundStyle(Color.blue)
                     }
+                    Spacer()
                 }
+                
             }
-            .alert(loginError?.localizedDescription ?? "Login error!", isPresented: $showingAlert) {
-                Button("OK", role: .cancel) { }
+            
+            Spacer()
+                
+            }
+        
+        MainButton(title: "Sign up") {
+            Task {
+                do {
+                    try await AuthService.shared.signUpUser(with: email, password: password)
+                    print("Signed up")
+                } catch {
+                    loginError = error
+                    showingAlert.toggle()
+                }
             }
         }
+        .alert(loginError?.localizedDescription ?? "Sign up error!", isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+        }
+        }
     }
-}
+
