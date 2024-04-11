f53103cbfed4f5a8f77965604aa8942e66e1d4c0
Spongebob229 <yyytimon@gmail.com>
2024-04-11 22:26:17 +0500

    postScreen

---
+++ Jina/Jina/PostCreationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  PostCreationScreen.swift
+//  Jina
+//
+//  Created by Schannikov Timothy on 11.04.2024.
+//
+
+import SwiftUI
+
+struct PostCreationScreen: View {
+    var body: some View {
+        Text()
+    }
+}
+
+#Preview {
+    PostCreationScreen()
+}
